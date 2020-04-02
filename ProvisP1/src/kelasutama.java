38dc15d2fe10c33eba49f6ba0339ab435c94416e
RadenFachrul <62990867+RadenFachrul@users.noreply.github.com>
2020-04-02 10:42:17 +0700

    Add files via upload

---
+++ ProvisP1/src/kelasutama.java
@@ -0,0 +1,24 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Yeojachingu
+ */
+public class kelasutama {
+    public static void main(String[] args) {
+        //System.out.println("Halo Dunia");
+        //menghidupkan objek
+        //frameutama futama = new frameutama();
+        //futama.tampil();
+        //futama2 ut = new futama2();
+       //ut.setVisible(true);
+        JOptionPane.showMessageDialog(null, "Selamat Belajar Provis");
+    }
+}
